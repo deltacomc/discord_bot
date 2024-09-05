a16fedd67cf123c218086b2b58cff7c99c1e814e
bufanda <thorsten@bufanda.de>
2024-09-05 09:30:14 +0200

    created bot and do some test commands

---
+++ main.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python3
+#####################
+# Discord Bot Test
+# 
+#
+#####################
+import os
+import random
+
+from discord.ext import commands
+import discord
+from dotenv import load_dotenv
+
+import discord.ext
+
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix="!",intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     brooklyn_99_quotes = [
+#         'I\'m the human form of the 💯 emoji.',
+#         'Bingpot!',
+#         (
+#             'Cool. Cool cool cool cool cool cool cool, '
+#             'no doubt no doubt no doubt no doubt.'
+#         ),
+#     ]
+
+#     if message.content == '99!':
+#         response = random.choice(brooklyn_99_quotes)
+#         await message.channel.send(response)
+
+@client.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@client.command(name='\/')
+async def spock(ctx):
+    star_trek_quotes = [
+        'Live long and prosper',
+        'To boldly go where no one has gone before',
+        (
+            'Heading,  '
+            'the third star from the left and follow the bow!'
+        ),
+    ]
+
+    response = random.choice(star_trek_quotes)
+    await ctx.send(response)
+
+
+@client.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+@client.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+client.run(TOKEN)
