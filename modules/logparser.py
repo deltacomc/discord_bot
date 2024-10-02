ac5debff36381450c516330f543052369eb47708
bufanda <thorsten@bufanda.de>
2024-10-02 20:25:02 +0200

    login parser: made username to accept every printable character

--- modules/logparser.py
+++ modules/logparser.py
@@ -29,7 +29,7 @@ class LoginParser(Parser):
     def __init__(self) -> None:
         # super().__init__()
         # pylint: disable=line-too-long
-        self.log_regex = r"^([0-9.-]*):\s'([0-9.]*)\s([0-9]*):([0-9A-Za-z]*)\([0-9]+\)'\slogged ([in|out]+)\sat:\sX=([0-9.-]*)\sY=([0-9.-]*)\sZ=([0-9.-]*)"
+        self.log_regex = r"^([0-9.-]*):\s'([0-9.]*)\s([0-9]*):(.+)\([0-9]+\)'\slogged ([in|out]+)\sat:\sX=([0-9.-]*)\sY=([0-9.-]*)\sZ=([0-9.-]*)"
         self.log_pattern = re.compile(self.log_regex)
         # pylint: enable=line-too-long
 
